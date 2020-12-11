775a3459c76a504fc655092b87d73fbbb407e92c
EugeneMaz10 <46795401+EugeneMaz10@users.noreply.github.com>
2020-12-11 11:19:45 +0300

    Update form.js

--- form.js
+++ form.js
@@ -1,8 +1,31 @@
+function nameCalculator(){
+    let d = document.getElementById('date').value;
+    let date = d.split('-')
+    // alert (date)
+    let year = (date[0])
+    // alert (year)
+    let month =parseInt(date[1])
+    //alert (month)
+    let day =parseInt(date[2])
+    //alert (date) 
+    let cc = parseInt(year.slice (0,2))
+    // alert (cc)
+    let yy = parseInt(year.slice(2,4))
+    // alert (yy)
+    let daysOfTheWeek = Math.floor ( ( ((cc/4) -2*cc-1) + ((5*yy/4) ) + ((26*(month+1)/10)) + day ) % 7)
+    // alert (daysOfTheWeek)
+    let gender = document.getElementById('gender'). value;
+    // alert(gender)
 
-console.log("Hello");
-var firstnumber = 12;
-var secondnumber = 14;
+    let maleNames= ['Kwasi', 'Kwadwo','Kwabena','Kwaku','Yaw', 'Kofi','Kwame']
+    let femaleNames=['Akosua','Adwowa','Abenaa','Akua','Yaa','Afua','Ama']
+    let dayy = ['Sunday','Monday','Tuesday','Wednesday','Thursday','Friday','Saturday']
 
-finalnumber = firstnumber+secondnumber;
+    if(gender === 'male'){
+        alert ('You were born on ' + dayy[daysOfTheWeek] + ' and your akan name is '+ maleNames[daysOfTheWeek])
+    }
 
-console.log(finalnumber);
+    else{
+        alert ('You were born on ' + dayy[daysOfTheWeek] + ' and your akan name is '+ femaleNames[daysOfTheWeek])
+    }
+}
