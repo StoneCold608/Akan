5229578acd88c04ce5caa03de57fff7e76f030be
EugeneMaz10 <46795401+EugeneMaz10@users.noreply.github.com>
2020-12-11 13:11:37 +0300

    Update form.js

--- form.js
+++ form.js
@@ -1,25 +1,26 @@
-function nameCalculator(){
-    let d = document.getElementById('date').value;
-    let date = d.split('-')
+function nameCalculator()
+{
+    d = document.getElementById('date').value;
+    date = d.split('-')
     // alert (date)
-    let year = (date[0])
+    year = (date[0])
     // alert (year)
-    let month =parseInt(date[1])
+    month =parseInt(date[1])
     //alert (month)
-    let day =parseInt(date[2])
+    day =parseInt(date[2])
     //alert (date) 
-    let cc = parseInt(year.slice (0,2))
+    cc = parseInt(year.slice (0,2))
     // alert (cc)
-    let yy = parseInt(year.slice(2,4))
+    yy = parseInt(year.slice(2,4))
     // alert (yy)
-    let daysOfTheWeek = Math.floor ( ( ((cc/4) -2*cc-1) + ((5*yy/4) ) + ((26*(month+1)/10)) + day ) % 7)
+    daysOfTheWeek = Math.floor ( ( ((cc/4) -2*cc-1) + ((5*yy/4) ) + ((26*(month+1)/10)) + day ) % 7)
     // alert (daysOfTheWeek)
-    let gender = document.getElementById('gender'). value;
+    gender = document.getElementById('gender'). value;
     // alert(gender)
 
-    let maleNames= ['Kwasi', 'Kwadwo','Kwabena','Kwaku','Yaw', 'Kofi','Kwame']
-    let femaleNames=['Akosua','Adwowa','Abenaa','Akua','Yaa','Afua','Ama']
-    let dayy = ['Sunday','Monday','Tuesday','Wednesday','Thursday','Friday','Saturday']
+    maleNames= ['Kwasi', 'Kwadwo','Kwabena','Kwaku','Yaw', 'Kofi','Kwame']
+    femaleNames=['Akosua','Adwowa','Abenaa','Akua','Yaa','Afua','Ama']
+    dayy = ['Sunday','Monday','Tuesday','Wednesday','Thursday','Friday','Saturday']
 
     if(gender === 'male'){
         alert ('You were born on ' + dayy[daysOfTheWeek] + ' and your akan name is '+ maleNames[daysOfTheWeek])
